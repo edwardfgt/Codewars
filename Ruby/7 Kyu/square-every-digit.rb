8fd4420c65de714faa2774e94b8d87ab8264306d
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-01-09 13:30:05 +0000

    fixed method error

---
+++ Ruby/7 Kyu/square-every-digit.rb
@@ -0,0 +1,11 @@
+# Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+# For example, if we run 9119 through the function, 811181 will come out, because 9^2 is 81 and 1^2 is 1.
+
+# Note: The function accepts an integer and returns an integer
+
+def square_digits(num)
+  newString = ""
+  num.to_s.each_char do |x| newString += (x_to.i ** 2).to_s end
+  newString.to_i
+end
