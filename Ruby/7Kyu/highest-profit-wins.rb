4bba429b71c93aa92738fe725066e4adfc6d34d2
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-01-09 13:43:04 +0000

    create highest-profit-wins.rb

--- Ruby/7Kyu/highest-profit-wins.rb
+++ Ruby/7Kyu/highest-profit-wins.rb
@@ -1,5 +1,5 @@
 # Write a function that returns both the minimum and maximum number of the given list/array. 
 
 def min_max(lst)
-  return [lst.min,lst.max] # fix me
+  return [lst.min,lst.max]
 end
